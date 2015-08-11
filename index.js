d0a5119cb4188a7a128150fa6f303d52c3b3ba41
Haimei <lihaimei880313@gmail.com>
2015-08-10 22:43:22 -0400

    add boy into page

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 var swipe = Swipe($("#content"));
+var getValue = function(className) {
+  var $elem = $('' + className + '');
+  return {
+    height: $elem.height(),
+    top: $elem.position().top
+  };
+};
 
-$('button').click(function() {
-  // call interface
-  swipe.scrollTo($('#content').width() * 2, 5000)
+var pathY = function() {
+  var data = getValue('.a_background_middle');
+  return data.top + data.height / 2;
+}();
+var $boy = $("#boy");
+var boyHeight = $boy.height();
+$boy.css({
+  top: pathY - boyHeight + 25
+});
+
+
+$("button").click(function() {
+  // left的变化
+  $boy.transition({
+    'left': $("#content").width() + 'px',
+  }, 10000, 'linear', function() {});
+
 });
